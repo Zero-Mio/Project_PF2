6dc4106a52d5f02249e4ab08a397e2e0b9b35261
Mio <epicly3pic@gmail.com>
2023-10-10 12:47:17 -0500

    Deleted rented and changed rented to borrowed

--- Book.java
+++ Book.java
@@ -51,16 +51,12 @@ public class Book {
 
     }
 
-    public boolean rented(){
-
-    }
-
     public static void main(String[] arguments) {
         // Small test of the Book class
         Book example = new Book("The Da Vinci Code");
         System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
         System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-        example.rented();
+        example.borrowed();
         System.out.println("Borrowed? (should be true): " + example.isBorrowed());
         example.returned();
         System.out.println("Borrowed? (should be false): " + example.isBorrowed());
