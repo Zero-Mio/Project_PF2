4f2caa9936c75b8c2594b8ac7d48abd67a8d2928
Mio <epicly3pic@gmail.com>
2023-11-09 11:53:22 -0600

    printAddress

--- Library.java
+++ Library.java
@@ -34,36 +34,40 @@ public class Library {
         System.out.println("Libraris are open daily from 9am to 5pm. ");
     }
 
+    public void printAddress() {
+        System.out.println(address);
+    }
+
     public void addBook(Book b){
         bookNames.add(b.getTitle());
     }
 
     public static void main(String[] args) {
-//        // Create two libraries
+        // Create two libraries
         Library firstLibrary = new Library("10 Main St.");
-//        Library secondLibrary = new Library("228 Liberty St.");
-//        Library thirdLibrary = new Library("12 Broadway St.", "catalog.txt");
-//
-//        // Add four books to the first library
+        Library secondLibrary = new Library("228 Liberty St.");
+        Library thirdLibrary = new Library("12 Broadway St.", "catalog.txt");
+
+        // Add four books to the first library
         firstLibrary.addBook(new Book("The Da Vinci Code"));
         firstLibrary.addBook(new Book("The Da Vinci Code")); // second copy
         firstLibrary.addBook(new Book("Le Petit Prince"));
         firstLibrary.addBook(new Book("A Tale of Two Cities"));
         firstLibrary.addBook(new Book("The Lord of the Rings"));
         firstLibrary.addBook(new Book("The Lord of the Rings")); // second copy
-//
-//        // Print opening hours and the addresses
-//        System.out.println("Library hours:");
-//        printOpeningHours();
-//        System.out.println();
 
-//        System.out.println("Library addresses:");
-//        firstLibrary.printAddress();
-//        secondLibrary.printAddress();
-//        thirdLibrary.printAddress();
-//        System.out.println();
-//
-//        // Try to borrow The Lords of the Rings from both libraries
+        // Print opening hours and the addresses
+        System.out.println("Library hours:");
+        printOpeningHours();
+        System.out.println();
+
+        System.out.println("Library addresses:");
+        firstLibrary.printAddress();
+        secondLibrary.printAddress();
+        thirdLibrary.printAddress();
+        System.out.println();
+
+        // Try to borrow The Lords of the Rings from both libraries
 //        System.out.println("Borrowing The Lord of the Rings:");
 //        firstLibrary.borrowBook("The Lord of the Rings");
 //        firstLibrary.borrowBook("The Lord of the Rings");
